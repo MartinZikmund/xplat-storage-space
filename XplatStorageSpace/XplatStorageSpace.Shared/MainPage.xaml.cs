f50adc7a5b0072170f9d67d995199ccb54b87f44
Martin Zikmund <martin@zikmund.dev>
2021-10-07 13:38:34 +0200

    Retrieve free space on Android

---
+++ XplatStorageSpace/XplatStorageSpace.Shared/MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Android.OS;
+using AndroidX.Core.Content;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Uno.UI;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace XplatStorageSpace
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnClick(object sender, RoutedEventArgs args)
+        {
+#if __ANDROID__
+            OutputInternalStorage();
+            OutputExternalStorage();
+#endif
+        }
+
+
+#if __ANDROID__
+        private void OutputInternalStorage()
+        {
+            var path = Android.OS.Environment.DataDirectory;
+            StatFs stat = new StatFs(path.Path);
+            long blockSize = stat.BlockSizeLong;
+            long availableBlocks = stat.AvailableBlocksLong;
+
+            long totalBlocks = stat.BlockCountLong;
+            System.Diagnostics.Debug.WriteLine("Internal free: " + formatSize(availableBlocks * blockSize));
+            System.Diagnostics.Debug.WriteLine("Internal total: " + formatSize(totalBlocks * blockSize));
+        }
+
+        private void OutputExternalStorage()
+        {
+            if (!ExternalMemoryAvailable())
+            {
+                return;
+            }
+
+            var path = ContextCompat.GetExternalFilesDirs(ContextHelper.Current, null)[1];
+            StatFs stat = new StatFs(path.Path);
+            long blockSize = stat.BlockSizeLong;
+            long availableBlocks = stat.AvailableBlocksLong;
+            long totalBlocks = stat.BlockCountLong;
+
+            System.Diagnostics.Debug.WriteLine("External free: " + formatSize(availableBlocks * blockSize));
+            System.Diagnostics.Debug.WriteLine("External total: " + formatSize(totalBlocks * blockSize));
+        }
+
+        private static bool ExternalMemoryAvailable()
+        {
+            return Android.OS.Environment.MediaMounted.Equals(Android.OS.Environment.ExternalStorageState, StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static string formatSize(long size)
+        {
+            string suffix = null;
+
+            if (size >= 1024)
+            {
+                suffix = "KB";
+                size /= 1024;
+                if (size >= 1024)
+                {
+                    suffix = "MB";
+                    size /= 1024;
+                }
+            }
+
+            StringBuilder resultBuffer = new StringBuilder(size.ToString());
+
+            int commaOffset = resultBuffer.Length - 3;
+            while (commaOffset > 0)
+            {
+                resultBuffer.Insert(commaOffset, ',');
+                commaOffset -= 3;
+            }
+
+            if (suffix != null) resultBuffer.Append(suffix);
+            return resultBuffer.ToString();
+        }
+
+#endif
+    }
+}
---
+++ XplatStorageSpace/XplatStorageSpace.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace XplatStorageSpace.Skia.Gtk
+{
+}
